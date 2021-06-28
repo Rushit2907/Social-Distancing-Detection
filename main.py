d88a04c91ce39a85df0805873921dfb119cfb934
Rushit Ajudiya <54739553+Rushit2907@users.noreply.github.com>
2021-06-28 14:46:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,54 @@ from centroidtracker import CentroidTracker
 from itertools import combinations
 
 click = 0
-mappedDist = 0
 points = []
+perspective_kernel = []
+distance_w = 0
+distance_h = 0
 protopath = "./model/MobileNetSSD_deploy.prototxt"
 modelpath = "./model/MobileNetSSD_deploy.caffemodel"
 
 detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
-tracker = CentroidTracker(maxDisappeared=80, maxDistance=90)
+tracker = CentroidTracker(maxDisappeared=70, maxDistance=80)
 
 # These are the classes of objects that can be detected by using the MobileNet SSD object detection algorithm
-CLASSES = ["background","aeroplane","bicycle","bird","boat","bottle","bus","car","cat","chair","cow","diningtable","dog","horse","motorbike","person","pottedplant","sheep","sofa","train","tvmonitor",]
+CLASSES = [
+    "background",
+    "aeroplane",
+    "bicycle",
+    "bird",
+    "boat",
+    "bottle",
+    "bus",
+    "car",
+    "cat",
+    "chair",
+    "cow",
+    "diningtable",
+    "dog",
+    "horse",
+    "motorbike",
+    "person",
+    "pottedplant",
+    "sheep",
+    "sofa",
+    "train",
+    "tvmonitor",
+]
 
-def calcDistance():
-    global points, mappedDist
-    mappedDist = (
-        (points[0][0] - points[1][0]) ** 2 + (points[0][1] - points[1][1]) ** 2
-    ) ** 0.5
+def cal_dis(p1, p2):
+    global distance_w, distance_h
+
+    h = abs(p2[1] - p1[1])
+    w = abs(p2[0] - p1[0])
+
+    dis_w = float((w / distance_w) * 180)
+    dis_h = float((h / distance_h) * 180)
+
+    return int(np.sqrt(((dis_h) ** 2) + ((dis_w) ** 2)))
 
 
+# To scale the bird's eye view window
 def get_scale(W, H):
 
     dis_w = 400
@@ -33,20 +63,36 @@ def get_scale(W, H):
     return float(dis_w / W), float(dis_h / H)
 
 
-
 def get_perspective_transform(H, W):
-    inp_point = np.float32(np.array(points[:4]))
-    dst_points = np.float32([[0, H], [W, H], [W, 0], [0, 0]])
+    global perspective_kernel, distance_w, distance_h
+    src = np.array(points[:4], dtype=np.float32)
+    dst = np.array([[0, H], [W, H], [W, 0], [0, 0]], dtype=np.float32)
 
     # Perspective convolution (3x3)
-    M = cv2.getPerspectiveTransform(inp_point, dst_points)
+    perspective_kernel = cv2.getPerspectiveTransform(src, dst)
 
     # Adjusting the original frame according to the perspective convolution M
-    perspective_transform = cv2.warpPerspective(frames, M, (H, W))
-    #cv2.imshow("perspective transform", perspective_transform)
+    perspective_transform = cv2.warpPerspective(frames, perspective_kernel, (H, W))
+    # cv2.imshow("perspective transform", perspective_transform)
+
+    pts = np.array([points[4:7]], dtype=np.float32)
+    warped_pt = cv2.perspectiveTransform(pts, perspective_kernel)[0]
+
+    # since bird eye view has property that all points are equidistant in horizontal and vertical direction.
+    # distance_w and distance_h will give us 180 cm distance in both horizontal and vertical directions
+    # (how many pixels will be there in 180cm length in horizontal and vertical direction of birds eye view),
+    # which we can use to calculate distance between two humans in transformed view or bird eye view
+    distance_w = (
+        (warped_pt[0][0] - warped_pt[1][0]) ** 2
+        + (warped_pt[0][1] - warped_pt[1][1]) ** 2
+    ) ** 0.5
+    distance_h = (
+        (warped_pt[0][0] - warped_pt[2][0]) ** 2
+        + (warped_pt[0][1] - warped_pt[2][1]) ** 2
+    ) ** 0.5
 
 
-def bird_eye_view(frames,red_mat,green_mat, scale_w, scale_h):
+def bird_eye_view(frames, red_mat, green_mat, scale_w, scale_h):
     h = frame.shape[0]
     w = frame.shape[1]
 
@@ -57,25 +103,48 @@ def bird_eye_view(frames,red_mat,green_mat, scale_w, scale_h):
     blank_image = np.zeros((int(h * scale_h), int(w * scale_w), 3), np.uint8)
     blank_image[:] = white
     for i in green_mat:
-        blank_image = cv2.circle(blank_image, (int(i[0] * scale_w), int(i[1] * scale_h)), 5, green, 10)
+        blank_image = cv2.circle(
+            blank_image, (int(i[0] * scale_w), int(i[1] * scale_h)), 5, green, 10
+        )
     for i in red_mat:
-        blank_image = cv2.circle(blank_image, (int(i[0] * scale_w), int(i[1] * scale_h)), 5, red, 10)
+        blank_image = cv2.circle(
+            blank_image, (int(i[0] * scale_w), int(i[1] * scale_h)), 5, red, 10
+        )
 
     return blank_image
 
+
 def get_points(event, x, y, flags, params):
-    global click, points, mappedDist
+    global click, points
 
     if event == cv2.EVENT_LBUTTONDOWN and click < 4:
         click += 1
         cv2.circle(frame, (x, y), 6, (255, 0, 0), -1)
 
         if len(points) == 3:
-            cv2.line(frame,(x, y),points[-1],(70, 70, 70),2,)
-            cv2.line(frame,(x, y),points[0],(70, 70, 70),2,)
+            cv2.line(
+                frame,
+                (x, y),
+                points[-1],
+                (70, 70, 70),
+                2,
+            )
+            cv2.line(
+                frame,
+                (x, y),
+                points[0],
+                (70, 70, 70),
+                2,
+            )
 
         elif len(points) >= 1:
-            cv2.line(frame,(x, y),points[-1],(70, 70, 70),2,)
+            cv2.line(
+                frame,
+                (x, y),
+                points[-1],
+                (70, 70, 70),
+                2,
+            )
 
         points.append((x, y))
         cv2.imshow("point selection", frame)
@@ -90,7 +159,7 @@ def get_points(event, x, y, flags, params):
         cv2.imshow("point selection", frame)
 
     elif click >= 7:
-        calcDistance()
+        # calcDistance()
         cv2.destroyWindow("point selection")
         return
 
@@ -138,16 +207,13 @@ def non_max_suppression_fast(boxes, overlapThresh):
 
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture("Videos/testvideo1.mp4")
+    cap = cv2.VideoCapture("./data/testvideo1.mp4")
 
     while True:
         ret, frame = cap.read()
-        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        # print(type(frame))
         H, W = frame.shape[0], frame.shape[1]
 
-        frame = imutils.resize(frame, width=width, height=height)
+        frame = imutils.resize(frame, width=W, height=H)
         scale_w, scale_h = get_scale(width, height)
 
         cv2.imshow("point selection", frame)
@@ -158,7 +224,7 @@ if __name__ == "__main__":
             cap.release()
             break
 
-    cap = cv2.VideoCapture("Videos/testvideo1.mp4")
+    cap = cv2.VideoCapture("data/testvideo1.mp4")
 
     # To calculate the FPS value we will the the start time and the end time and then subtract the end time from the
     # start time and dividing that by the number of frames we will get the value of the FPS
@@ -177,14 +243,12 @@ if __name__ == "__main__":
 
     while True:
         ret, frames = cap.read()
-        # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        # frames = imutils.resize(frames, width=600)
         total_frames += 1
         fps_end_time = datetime.datetime.now()
 
         # Calculating the height and the width of the frame
         H, W = frames.shape[:2]
+        get_perspective_transform(H, W)
         key = cv2.waitKey(1)
         if key == ord("q"):
             break
@@ -250,9 +314,6 @@ if __name__ == "__main__":
 
             centroid_dict[objectId] = (cX, cY, x1, y1, x2, y2)
 
-            # text = "ID: {}".format(objectId)
-            # cv2.putText(frames, text, (x1, y1 - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
-
         # This list will hold all the bounding box which are not at a safe distance from each other
         red_zone_list = []
 
@@ -260,10 +321,9 @@ if __name__ == "__main__":
         # is greater than some threshold value then the bounding box will be green else it will be red
 
         for (id1, p1), (id2, p2) in combinations(centroid_dict.items(), 2):
-            dx, dy = p1[0] - p2[0], p1[1] - p2[1]
-            distance = math.sqrt(dx * dx + dy * dy)
-
-            if distance < 75.0:
+            # converts pixel distace (manhatan distance) to cm.
+            distance = cal_dis(p1, p2)
+            if distance < 180.0:
                 if id1 not in red_zone_list:
                     red_zone_list.append(id1)
 
@@ -275,13 +335,29 @@ if __name__ == "__main__":
         # Now we will display the bounding boxes accordingly
         for id, box in centroid_dict.items():
             if id in red_zone_list:
-                cv2.circle(frames,((box[2]+box[4])//2,box[5]),10,(0,0,255),-1)
-                red_mat.append(((box[2]+box[4])//2,box[5]))
-                cv2.rectangle(frames, (box[2], box[3]), (box[4], box[5]), (0, 0, 255), 2)
+                cv2.circle(
+                    frames, ((box[2] + box[4]) // 2, box[5]), 10, (0, 0, 255), -1
+                )
+                tmp = np.array([[[(box[2] + box[4]) // 2, box[5]]]], dtype=np.float32)
+                perspective_pnt = cv2.perspectiveTransform(tmp, perspective_kernel)[0][
+                    0
+                ]
+                red_mat.append(perspective_pnt)
+                cv2.rectangle(
+                    frames, (box[2], box[3]), (box[4], box[5]), (0, 0, 255), 2
+                )
             else:
-                cv2.circle(frames,((box[2]+box[4])//2,box[5]),10,(0,255,0),-1)
-                green_mat.append(((box[2]+box[4])//2,box[5]))
-                cv2.rectangle(frames, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2)
+                cv2.circle(
+                    frames, ((box[2] + box[4]) // 2, box[5]), 10, (0, 255, 0), -1
+                )
+                tmp = np.array([[[(box[2] + box[4]) // 2, box[5]]]], dtype=np.float32)
+                perspective_pnt = cv2.perspectiveTransform(tmp, perspective_kernel)[0][
+                    0
+                ]
+                green_mat.append(perspective_pnt)
+                cv2.rectangle(
+                    frames, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2
+                )
 
         # Calculating the time difference by subtracting the start time from the end time
         time_diff = fps_end_time - fps_start_time
@@ -296,15 +372,15 @@ if __name__ == "__main__":
             frames,
             fps_text,
             (5, 30),
-            cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+            cv2.FONT_HERSHEY_TRIPLEX,
             1,
             (0, 0, 255),
             1,
         )
         cv2.imshow("Application", frames)
-        get_perspective_transform(H, W)
-        image = bird_eye_view(frames,red_mat,green_mat,scale_w,scale_h)
-        cv2.imshow("Bird's EYE",image)
+        # get_perspective_transform(H, W)
+        image = bird_eye_view(frames, red_mat, green_mat, scale_w, scale_h)
+        cv2.imshow("Bird's EYE", image)
 
     cap.release()
     cv2.destroyAllWindows()
